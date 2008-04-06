7402b7b7086745b0688e28e95f687df940699ae0
christian.kakesa <christian.kakesa@4013a557-244a-0410-83ab-652c19c8c112>
2008-04-06 00:57:06 +0000

    First commit on google code svn.

---
+++ config.rb
@@ -0,0 +1,50 @@
+=begin
+  Constants files for RSOULng.
+=end
+begin
+  require 'socket'
+  require 'yaml'
+  require 'digest/md5'
+  require 'uri'
+  require 'open-uri'
+  require 'gtk2'
+  require 'logger'
+  require 'thread'
+  require 'ping'
+  require 'gtktrayicon'
+rescue LoadError
+end
+#include Gtk
+## APPLICATION CONFIG ##
+########################
+RS_APP_NAME = "RubySoul-NG"
+RS_VERSION = "0.1.1a"
+RS_AUTHOR_NAME = "Christian"
+RS_AUTHOR_FIRSTNAME = "KAKESA"
+RS_AUTHOR_EMAIL = "christian.kakesa@etna-alternance.net"
+RS_AGENT = RS_APP_NAME + " - V" + RS_VERSION
+RS_DEFAULT_SIZE_W = 300
+RS_DEFAULT_SIZE_H = 600
+RS_DEFAULT_THEME = "msn"
+## SERVER AND PORT CONFIG ##
+############################
+RS_HOST = "ns-server.epita.fr"
+RS_PORT = 4242
+## ICONS and IMAGES ##
+######################
+RS_IMG_BG = File.dirname(__FILE__) + File::SEPARATOR + "images" + File::SEPARATOR + "background" + File::SEPARATOR + "etna_long.png"
+RS_IMG_LOGO = File.dirname(__FILE__)  + File::SEPARATOR + "images" + File::SEPARATOR + "themes" + File::SEPARATOR + "" + RS_DEFAULT_THEME + "" + File::SEPARATOR + "logo" + File::SEPARATOR + "MSN Logo.png"
+RS_IMG_CONNECT = File.dirname(__FILE__) + File::SEPARATOR + "images" + File::SEPARATOR + "themes" + File::SEPARATOR + RS_DEFAULT_THEME + File::SEPARATOR + "state" + File::SEPARATOR + "Online.png"
+RS_IMG_MULTICONNECT = File.dirname(__FILE__) + File::SEPARATOR + "images" + File::SEPARATOR + "themes" + File::SEPARATOR + RS_DEFAULT_THEME + File::SEPARATOR + "state" + File::SEPARATOR + "Web.png"
+RS_IMG_DISCONNECT = File.dirname(__FILE__) + File::SEPARATOR + "images" + File::SEPARATOR + "themes" + File::SEPARATOR + RS_DEFAULT_THEME + File::SEPARATOR + "state" + File::SEPARATOR + "Offline.png"
+RS_IMG_SEND_MSG = File.dirname(__FILE__) + File::SEPARATOR + "images" + File::SEPARATOR + "themes" + File::SEPARATOR + RS_DEFAULT_THEME + File::SEPARATOR  + "logo" + File::SEPARATOR + "MSN Messenger.png"
+RS_IMG_CONTACT = File.dirname(__FILE__) + File::SEPARATOR + "images" + File::SEPARATOR + "themes" + File::SEPARATOR + RS_DEFAULT_THEME + File::SEPARATOR + "logo" + File::SEPARATOR + "MSN Contacts.png"
+RS_ICON_STATE_ACTIVE = File.dirname(__FILE__) + File::SEPARATOR + "images" + File::SEPARATOR + "themes" + File::SEPARATOR + RS_DEFAULT_THEME + File::SEPARATOR + "state" + File::SEPARATOR + "Online.png"
+RS_ICON_STATE_AWAY = File.dirname(__FILE__) + File::SEPARATOR + "images" + File::SEPARATOR + "themes" + File::SEPARATOR + RS_DEFAULT_THEME + File::SEPARATOR + "state" + File::SEPARATOR + "Away.png"
+RS_ICON_STATE_IDLE = File.dirname(__FILE__) + File::SEPARATOR + "images" + File::SEPARATOR + "themes" + File::SEPARATOR + RS_DEFAULT_THEME + File::SEPARATOR + "state" + File::SEPARATOR + "Away.png"
+RS_ICON_STATE_LOCK = File.dirname(__FILE__) + File::SEPARATOR + "images" + File::SEPARATOR + "themes" + File::SEPARATOR + RS_DEFAULT_THEME + File::SEPARATOR + "state" + File::SEPARATOR + "Blocked.png"
+RS_ICON_STATE_SERVER = File.dirname(__FILE__) + File::SEPARATOR + "images" + File::SEPARATOR + "themes" + File::SEPARATOR + RS_DEFAULT_THEME + File::SEPARATOR + "state" + File::SEPARATOR + "Blocked.png"
+RS_ICON_STATE_DISCONNECT = File.dirname(__FILE__) + File::SEPARATOR + "images" + File::SEPARATOR + "themes" + File::SEPARATOR + RS_DEFAULT_THEME + File::SEPARATOR + "state" + File::SEPARATOR + "Offline.png"
+## Main Frame Props ##
+######################
+## RS_FRAME_BG_COLOR = Gdk::Color.new(21157, 45372, 41804)
