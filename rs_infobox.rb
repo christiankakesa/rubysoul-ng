d726aecb9d5a1c40cdfd5d8d71f9d2b7ebf310af
christian.kakesa <christian.kakesa@4013a557-244a-0410-83ab-652c19c8c112>
2008-11-03 11:31:03 +0000

    Add logo.png in root dir. Add the glade file. Add RsInfobox class for message dialog (info, warning, error...)

---
+++ rs_infobox.rb
@@ -0,0 +1,34 @@
+=begin
+  Made by Christian KAKESA etna_2008(paris) <christian.kakesa@gmail.com>
+=end
+
+begin
+  require 'gtk2'
+rescue LoadError
+  puts "Error: #{$!}"
+  exit
+end
+
+class RsInfobox
+  def initialize(main_app_window, message, type = "info")
+    info_type = Gtk::MessageDialog::OTHER
+    info_button = Gtk::MessageDialog::BUTTONS_CLOSE
+    case type.to_s
+    when "error"
+      info_type = Gtk::MessageDialog::ERROR
+    when "info"
+      info_type = Gtk::MessageDialog::INFO
+      info_button = Gtk::MessageDialog::BUTTONS_OK
+    when "question"
+      info_type = Gtk::MessageDialog::QUESTION
+      info_button = Gtk::MessageDialog::BUTTONS_YES_NO
+    when "warning"
+      info_type = Gtk::MessageDialog::WARNING
+      info_button = Gtk::MessageDialog::BUTTONS_OK
+    end
+    infobox = Gtk::MessageDialog.new(main_app_window, Gtk::Dialog::MODAL, info_type, info_button, message.to_s)
+    infobox.run()
+    infobox.destroy()
+  end
+end
+
