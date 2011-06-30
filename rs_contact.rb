e4831f55ed63c569fd88090318acdb9307500c29
christian.kakesa@gmail.com <christian.kakesa@gmail.com@4013a557-244a-0410-83ab-652c19c8c112>
2011-06-30 19:58:24 +0000

    Source code beautifer

--- rs_contact.rb
+++ rs_contact.rb
@@ -1,115 +1,115 @@
 =begin
-  Made by Christian KAKESA etna_2008(paris) <christian.kakesa@gmail.com>
+	Made by Christian KAKESA etna_2008(paris) <christian.kakesa@gmail.com>
 =end
 
 begin
-  require 'open-uri'
-  require 'singleton'
-  require 'rs_infobox'
+	require 'open-uri'
+	require 'singleton'
+	require 'rs_infobox'
 rescue LoadError
-  puts "Error: #{$!}"
-  exit
+	puts "Error: #{$!}"
+	exit
 end
 
 class RsContact
-  include Singleton
+	include Singleton
 
-  attr_accessor :contacts, :url_photo
+	attr_accessor :contacts, :url_photo
 
-  def initialize()
-    @rs_config = RsConfig::instance()
-    @url_photo = @rs_config.contacts_photo_url #--- | chck if are in PIE for locale url : http://intra/photo.php?login=
-    load_contacts()
-  end
+	def initialize()
+		@rs_config = RsConfig::instance()
+		@url_photo = @rs_config.contacts_photo_url #--- | chck if are in PIE for locale url : http://intra/photo.php?login=
+		load_contacts()
+	end
 
-  def load_contacts
-    @contacts = YAML::load_file(@rs_config.contacts_filename)
-    if not @contacts.is_a?(Hash)
-      @contacts = Hash.new
-    end
-  end
-  #--- Add login to the YML contact file.
-  def add(login, save_it = false)
-    if not (@contacts.include?(login.to_sym))
-      total_length = get_users_list().to_s.length + login.to_s.length
-      if total_length <= 1022 # 1022 is limit of netsoul watch_log_user command
-        @contacts[login.to_sym] = Hash.new
-        save() if save_it
-      else
-        raise(StandardError, "NetSoul server is not able to manage more contacts status for you.\nRemove one or more contacts before adding another.\nThis limitation is server limit, sorry.")
-      end
-    end
-  end
+	def load_contacts
+		@contacts = YAML::load_file(@rs_config.contacts_filename)
+		if not @contacts.is_a?(Hash)
+			@contacts = Hash.new
+		end
+	end
+	#--- Add login to the YML contact file.
+	def add(login, save_it = false)
+		if not (@contacts.include?(login.to_sym))
+			total_length = get_users_list().to_s.length + login.to_s.length
+			if total_length <= 1022 # 1022 is limit of netsoul watch_log_user command
+				@contacts[login.to_sym] = Hash.new
+				save() if save_it
+			else
+				raise(StandardError, "NetSoul server is not able to manage more contacts status for you.\nRemove one or more contacts before adding another.\nThis limitation is server limit, sorry.")
+			end
+		end
+	end
 
-  #--- Remove contact to the YML file.
-  def remove(login, save_it = false)
-    @contacts.delete(login.to_s.to_sym)
-    if FileTest.exist?(@rs_config.contacts_photo_dir+File::SEPARATOR+login.to_s)
-      begin
-        File.delete(@rs_config.contacts_photo_dir+File::SEPARATOR+login.to_s)
-      rescue
-        RsInfobox.new(@parent_win, "#{$!}", "warning")
-      end
-    end
-    save() if save_it
-  end
+	#--- Remove contact to the YML file.
+	def remove(login, save_it = false)
+		@contacts.delete(login.to_s.to_sym)
+		if FileTest.exist?(@rs_config.contacts_photo_dir+File::SEPARATOR+login.to_s)
+			begin
+				File.delete(@rs_config.contacts_photo_dir+File::SEPARATOR+login.to_s)
+			rescue
+				RsInfobox.new(@parent_win, "#{$!}", "warning")
+			end
+		end
+		save() if save_it
+	end
 
-  #--- Save contact hash table to the YAML file
-  def save
-    c = Hash.new
-    if @contacts.length > 0
-      @contacts.keys.uniq.each do |l|
-        c[l.to_s.to_sym] = Hash.new
-      end
-    end
-    File.open(@rs_config.contacts_filename, "wb") do |file|
-      file.puts '#--- ! RubySoulNG contacts file'
-      file.puts c.to_yaml
-    end
-  end
+	#--- Save contact hash table to the YAML file
+	def save
+		c = Hash.new
+		if @contacts.length > 0
+			@contacts.keys.uniq.each do |l|
+				c[l.to_s.to_sym] = Hash.new
+			end
+		end
+		File.open(@rs_config.contacts_filename, "wb") do |file|
+			file.puts '#--- ! RubySoulNG contacts file'
+			file.puts c.to_yaml
+		end
+	end
 
-  def get_users_list
-    user_list = String.new
-    @contacts.each do |k, v|
-      user_list += k.to_s + ","
-    end
-    user_list = user_list.slice(0, user_list.length - 1)
-    return user_list
-  end
+	def get_users_list
+		user_list = String.new
+		@contacts.each do |k, v|
+			user_list += k.to_s + ","
+		end
+		user_list = user_list.slice(0, user_list.length - 1)
+		return user_list
+	end
 
-  def get_users_photo
-    dest_dir = @rs_config.contacts_photo_dir
-    files = Array.new
-    exclude_dir = [".", ".."]
-    lf = Dir.open(dest_dir)
-    liste = lf.sort - exclude_dir
-    lf.close
-    liste.each do |f|
-      if (File.ftype(dest_dir + File::SEPARATOR + f) == "file")
-        files << f.to_s
-      end
-    end
-    @contacts.each do |k, v|
-      if not (files.include?(k.to_s))
-        get_user_photo(k)
-      end
-    end
-  end
-  
-  def get_user_photo(login)
+	def get_users_photo
+		dest_dir = @rs_config.contacts_photo_dir
+		files = Array.new
+		exclude_dir = [".", ".."]
+		lf = Dir.open(dest_dir)
+		liste = lf.sort - exclude_dir
+		lf.close
+		liste.each do |f|
+			if (File.ftype(dest_dir + File::SEPARATOR + f) == "file")
+				files << f.to_s
+			end
+		end
+		@contacts.each do |k, v|
+			if not (files.include?(k.to_s))
+				get_user_photo(k)
+			end
+		end
+	end
+	
+	def get_user_photo(login)
 	begin
-      hh = open(@url_photo + login.to_s, "rb")
-    rescue
-      puts "Error: #{$!}"
-    end
-    if (hh)
-      h = File.open(@rs_config.contacts_photo_dir + File::SEPARATOR + login.to_s, "wb")
-      if (h)
-        h.write(hh.read)
-        h.close
-      end
-      hh.close
-    end
-  end
+			hh = open(@url_photo + login.to_s, "rb")
+		rescue
+			puts "Error: #{$!}"
+		end
+		if (hh)
+			h = File.open(@rs_config.contacts_photo_dir + File::SEPARATOR + login.to_s, "wb")
+			if (h)
+				h.write(hh.read)
+				h.close
+			end
+			hh.close
+		end
+	end
 end
 
--- rs_status_icon.rb
+++ rs_status_icon.rb
@@ -1,91 +1,91 @@
 =begin
-  Made by Christian KAKESA etna_2008(paris) <christian.kakesa@gmail.com>
+	Made by Christian KAKESA etna_2008(paris) <christian.kakesa@gmail.com>
 =end
 
 begin
-  require 'gtk2'
-  require 'rs_config'
+	require 'gtk2'
+	require 'rs_config'
 rescue LoadError
-  puts "Error: #{$!}"
-  exit
+	puts "Error: #{$!}"
+	exit
 end
 
 class RsStatusIcon < Gtk::StatusIcon
 
-  def initialize(main_app, parent_win = nil)
-    super()
-    @main_app = main_app
-    @parent_win = parent_win
-    build_status_icon()
+	def initialize(main_app, parent_win = nil)
+		super()
+		@main_app = main_app
+		@parent_win = parent_win
+		build_status_icon()
 
-    signal_connect("activate") do |w|
-      unless @parent_win.visible?
-        @parent_win.show_all()
-      else
-        if @parent_win.active?
-          @parent_win.hide_all()
-        else
-          @parent_win.present()
-        end
-      end
-    end
-    signal_connect("popup-menu") do |widget, button, activate_time|
-      menu = Gtk::Menu.new
-      menuitem = Gtk::ImageMenuItem.new(Gtk::Stock::CONNECT)
-      menuitem.signal_connect("activate"){|w|
-        @main_app.on_tb_connect_clicked(w)
-      }
-      menu.append(menuitem)
+		signal_connect("activate") do |w|
+			unless @parent_win.visible?
+				@parent_win.show_all()
+			else
+				if @parent_win.active?
+					@parent_win.hide_all()
+				else
+					@parent_win.present()
+				end
+			end
+		end
+		signal_connect("popup-menu") do |widget, button, activate_time|
+			menu = Gtk::Menu.new
+			menuitem = Gtk::ImageMenuItem.new(Gtk::Stock::CONNECT)
+			menuitem.signal_connect("activate"){|w|
+				@main_app.on_tb_connect_clicked(w)
+			}
+			menu.append(menuitem)
 
-      menuitem = Gtk::ImageMenuItem.new(Gtk::Stock::DISCONNECT)
-      menuitem.signal_connect("activate"){|w|
-        @main_app.on_tb_connect_clicked(w)
-      }
-      menu.append(menuitem)
-      
-      menu.append(Gtk::SeparatorMenuItem.new)
+			menuitem = Gtk::ImageMenuItem.new(Gtk::Stock::DISCONNECT)
+			menuitem.signal_connect("activate"){|w|
+				@main_app.on_tb_connect_clicked(w)
+			}
+			menu.append(menuitem)
+			
+			menu.append(Gtk::SeparatorMenuItem.new)
 
-      menuitem = Gtk::ImageMenuItem.new(Gtk::Stock::ADD)
-      menuitem.child().set_label("C_ontacts")
-      menuitem.signal_connect("activate"){|w|
-        @main_app.on_tb_contact_clicked(w)
-      }
-      menu.append(menuitem)
+			menuitem = Gtk::ImageMenuItem.new(Gtk::Stock::ADD)
+			menuitem.child().set_label("C_ontacts")
+			menuitem.signal_connect("activate"){|w|
+				@main_app.on_tb_contact_clicked(w)
+			}
+			menu.append(menuitem)
 
-      menuitem = Gtk::ImageMenuItem.new(Gtk::Stock::PREFERENCES)
-      menuitem.signal_connect("activate"){|w|
-        @main_app.on_tb_preferences_clicked(w)
-      }
-      menu.append(menuitem)
-      
-      menu.append(Gtk::SeparatorMenuItem.new)
+			menuitem = Gtk::ImageMenuItem.new(Gtk::Stock::PREFERENCES)
+			menuitem.signal_connect("activate"){|w|
+				@main_app.on_tb_preferences_clicked(w)
+			}
+			menu.append(menuitem)
+			
+			menu.append(Gtk::SeparatorMenuItem.new)
 
-      menuitem = Gtk::ImageMenuItem.new(Gtk::Stock::ABOUT)
-      menuitem.child().set_label("_Informations")
-      menuitem.signal_connect("activate"){|w|
-        @main_app.on_tb_about_clicked(w)
-      }
-      menu.append(menuitem)
+			menuitem = Gtk::ImageMenuItem.new(Gtk::Stock::ABOUT)
+			menuitem.child().set_label("_Informations")
+			menuitem.signal_connect("activate"){|w|
+				@main_app.on_tb_about_clicked(w)
+			}
+			menu.append(menuitem)
 
 			menu.append(Gtk::SeparatorMenuItem.new)
 			
-      menuitem = Gtk::ImageMenuItem.new(Gtk::Stock::QUIT)
-      menuitem.signal_connect("activate"){
-        set_visible(false)
-        @main_app.on_statusicon_delete_event()
-      }
-      menu.append(menuitem)
+			menuitem = Gtk::ImageMenuItem.new(Gtk::Stock::QUIT)
+			menuitem.signal_connect("activate"){
+				set_visible(false)
+				@main_app.on_statusicon_delete_event()
+			}
+			menu.append(menuitem)
 
-      menu.show_all
-      menu.popup(nil, nil, button, activate_time)
-    end
-  end
+			menu.show_all
+			menu.popup(nil, nil, button, activate_time)
+		end
+	end
 
-  def build_status_icon
-    #set_icon_name(Gtk::Stock::DIALOG_INFO)
-    set_pixbuf(Gdk::Pixbuf.new("#{RsConfig::APP_DIR+File::SEPARATOR}logo.png"))
-    set_tooltip("#{RsConfig::APP_NAME} #{RsConfig::APP_VERSION}")
-    set_visible(true)
-  end
+	def build_status_icon
+		#set_icon_name(Gtk::Stock::DIALOG_INFO)
+		set_pixbuf(Gdk::Pixbuf.new("#{RsConfig::APP_DIR+File::SEPARATOR}logo.png"))
+		set_tooltip("#{RsConfig::APP_NAME} #{RsConfig::APP_VERSION}")
+		set_visible(true)
+	end
 end
 
